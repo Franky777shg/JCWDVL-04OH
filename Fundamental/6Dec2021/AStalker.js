09be0a72841198f6f552623fd83ced7b85ea373f
Franky777shg <frengky.sihombing.777@gmail.com>
2021-12-08 13:10:01 +0700

    latihan soal variabel dan tipe data

---
+++ Fundamental/6Dec2021/AStalker.js
@@ -0,0 +1,4 @@
+let word = 'atlAntica'
+
+let result = word.replace(/a/g, 'a.').replace(/A/g, 'A.')
+console.log(result)
---
+++ Fundamental/6Dec2021/MultiplyFive.js
@@ -0,0 +1,4 @@
+let start = -10
+
+let result = start * (5 ** Math.abs(start).toString().length)
+console.log(result)
