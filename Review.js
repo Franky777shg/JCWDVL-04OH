87e22d8aa6e24d92fa5e33116d50bc7640580501
Frengky Sihombing <64943858+Franky777shg@users.noreply.github.com>
2021-12-13 16:09:50 +0700

    Practice Session Array

---
+++ Review.js
@@ -0,0 +1,5 @@
+let arr = [1, 2, 3, 4];
+arr.sort();
+
+// Method sort itu tidak serta merta langsung ada, tapi diciptakan yang tentunya dengan algoritma yang dibuat secara manual.
+// Algoritma yang diciptakan secara manual ini lah yang membuat method sort bekerja.
